47ccd978e5777b337f85ddec54b04340bacc9b81
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-11-01 12:27:38 -0500

    rm next

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -8,7 +9,7 @@ app.use(express.json());
 
 app.use('/', routes);
 
-app.use((err, req, res, next) => {
+app.use((err, req, res) => {
   console.error(err.stack);
   res.status(500).json({ error: 'Internal Server Error' });
 });
