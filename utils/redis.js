34d1d4df1eeb6d027fe38db4ccde5b832268bce0
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-11-01 10:07:00 -0500

    display redis erros in console

--- utils/redis.js
+++ utils/redis.js
@@ -1,7 +1,12 @@
 const Redis = require('ioredis')
 
 class RedisClient {
-
+  constructor() {
+    this.client = new Redis();
+    this.client.offlineQueue('error', (err) => {
+      console.error('Redis client error:' err);
+    })
+  }
 }
 
 module.exports = { RedisClient, redisClient };
