38ac8863ffe807ca829c82b8a4cdb50732da623c
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-11-02 13:03:05 -0500

    add mongo credintials to connect to client

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,3 +1,9 @@
 import express from 'express';
 import crypto from 'crypto';
 import { MongoClient } from 'mongodb';
+
+const router = express.Router();
+const uri = ''
+const client = new MongoClient(uri);
+
+client.connect();
