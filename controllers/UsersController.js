4f49da8f41a53901c7562bb56164c701a6c1c7b9
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-11-02 16:43:07 -0500

    add getMe that gets token from req heade, decodes token, finds user in db using decoded id, and returns only the emial and id

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -31,6 +31,36 @@ class UserController {
 
     res.status(201).json({ id: result.insertedId, email });
   }
+
+  static async getMe(req, res) {
+    try {
+      // get token from req headersss
+      const token = req.headers['authorization'];
+      if (!token) {
+        return res.status(401).json({ error: 'Unauthorized' });
+      }
+      // decode token
+      const decodedToken = jwt.decode(token);
+      if (!decodedToken || !decodedToken.id) {
+        return res.status(401).json({ error: 'Invalid token '});
+      }
+      // find usr in db using decoded id
+      const usersCollection = dbClient.db.collection('users');
+      const user = await usersCollection.findOne({ _id: decodedToken.id });
+      if (!user) {
+        return res.status(404).json({ error: 'User not found' });
+      }
+      // return only email & id
+      const userData = {
+        email: user.email,
+        id: user._id.toString()
+      };
+      res.status(200).json(userData);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  }
 }
 
 export default UserController;
