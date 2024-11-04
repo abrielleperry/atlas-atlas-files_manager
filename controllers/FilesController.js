3e73131ac363e64662e1bbac1afec1e12d288f2b
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-11-04 14:15:57 -0600

    fix error

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -1,55 +1,55 @@
-import { v4: uuidv4 } = require('uuid');
-import fs = require('fs');
-import path = require('path');
-import db = require('../db');
+import { v4 as uuidv4 } from 'uuid';
+import fs from 'fs';
+import path from 'path';
+import db from '../utils/db';
 
+// sets the folder path where files will be stored
 const FOLDER_PATH = process.env.FOLDER_PATH || '/tmp/files_manager';
 
 class FilesController {
   static async postUpload(req, res) {
-    const { name, type, parentId = 0, isPublic = false, data } = req.body;
-    const userId = req.user.id;
+  const { name, type, parentId = 0, isPublic = false, data } = req.body;
+  const userId = req.user.id;
 
     if (!name) {
-      return res.status(400).json({ error: 'Missing name'})
+      return res.status(400).json({ error: 'Missing name' });
     }
 
     if (!['folder', 'file', 'image'].includes(type)) {
-      return res.status(400).json({ error: 'Missing type' })
+      return res.status(400).json({ error: 'Missing type' });
     }
 
     if (type !== 'folder' && !data) {
-      return res.status(400).json({error: 'Missing data'})
+      return res.status(400).json({ error: 'Missing data' });
     }
 
     if (parentId) {
       const parentFile = await db.getFileById(parentId);
       if (!parentFile) {
-        return res.status(400).json({ error: 'Parent not found' })
+        return res.status(400).json({ error: 'Parent not found' });
       }
       if (parentFile.type !== 'folder') {
-        return res.status(400).json({ error: 'Parent is not a folder' })
+        return res.status(400).json({ error: 'Parent is not a folder' });
       }
     }
 
     const newFile = {
-      userId, name, type, isPublic, parentId
+      userId, name, type, isPublic, parentId,
     };
 
     if (type === 'folder') {
       const createdFile = await db.createdFile(newFile);
       return res.status(201).json(createdFile);
-    } else {
-      const filePath = path.join(FOLDER_PATH, uuidv4());
-      const fileBuffer = Buffer.from(data, 'base64');
-
-      fs.mkdirSync(FOLDER_PATH, { recursive : true });
-      fs.writeFileSync(filePath, fileBuffer);
-
-      newFile.localPath = filePath;
-      const createdFile = await db.createdFile(newFile);
-      return res.status(201).json(createdFile)
     }
+    const filePath = path.join(FOLDER_PATH, uuidv4());
+    const fileBuffer = Buffer.from(data, 'base64');
+
+    fs.mkdirSync(FOLDER_PATH, { recursive: true });
+    fs.writeFileSync(filePath, fileBuffer);
+
+    newFile.localPath = filePath;
+    const createdFile = await db.createdFile(newFile);
+    return res.status(201).json(createdFile);
   }
 }
 
